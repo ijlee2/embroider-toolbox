79beb1049fa798855d5fa816960a8a3efac6c6aa
ijlee2 <ijlee2@users.noreply.github.com>
2024-08-14 22:25:09 +0200

    feature: Guaranteed idempotency

--- packages/blueprints-addon/src/run-new.ts
+++ packages/blueprints-addon/src/run-new.ts
@@ -1,8 +1,21 @@
+import { existsSync } from 'node:fs';
+import { join } from 'node:path';
+
 import { createAddon, createOptions } from './steps/run-new/index.js';
-import type { CodemodOptions } from './types/run-new.js';
+import type { CodemodOptions, Options } from './types/run-new.js';
+
+function addonExists(options: Options): boolean {
+  const { addon, projectRoot } = options;
+
+  return existsSync(join(projectRoot, addon.location, 'package.json'));
+}
 
 export function runNew(codemodOptions: CodemodOptions): void {
   const options = createOptions(codemodOptions);
+
+  if (addonExists(options)) {
+    return;
+  }
 
   createAddon(options);
 }
