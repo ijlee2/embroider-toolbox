a364af5c146c2528636ea93e06507995d4d6f534
ijlee2 <ijlee2@users.noreply.github.com>
2024-08-14 15:32:45 +0200

    feature: Created create-files-from-blueprints

--- packages/create-v2-addon-repo/src/index.ts
+++ packages/create-v2-addon-repo/src/index.ts
@@ -1,9 +1,8 @@
-import { createOptions } from './steps/index.js';
+import { createFilesFromBlueprints, createOptions } from './steps/index.js';
 import type { CodemodOptions } from './types/index.js';
 
 export function runCodemod(codemodOptions: CodemodOptions): void {
   const options = createOptions(codemodOptions);
 
-  // TODO: Replace with actual steps
-  console.log(options);
+  createFilesFromBlueprints(options);
 }
