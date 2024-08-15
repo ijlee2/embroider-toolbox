a939c01b043371ae7337782f6973028404b1862f
ijlee2 <ijlee2@users.noreply.github.com>
2024-08-15 14:04:17 +0200

    feature: Guaranteed idempotency

--- packages/blueprints-addon/src/run-generate.ts
+++ packages/blueprints-addon/src/run-generate.ts
@@ -1,8 +1,26 @@
+import { join } from 'node:path';
+
+import { findFiles } from '@codemod-utils/files';
+
 import { createEntity, createOptions } from './steps/run-generate/index.js';
-import type { CodemodOptions } from './types/run-generate.js';
+import type { CodemodOptions, Options } from './types/run-generate.js';
+
+function entityExists(options: Options): boolean {
+  const { entity, projectRoot } = options;
+
+  const files = findFiles(join('src', `${entity.type}s`, `${entity.name}.*`), {
+    projectRoot,
+  });
+
+  return files.length > 0;
+}
 
 export function runGenerate(codemodOptions: CodemodOptions): void {
   const options = createOptions(codemodOptions);
+
+  if (entityExists(options)) {
+    return;
+  }
 
   createEntity(options);
 }
