97c93f88d057bb7c8c4623997016249051f65198
ijlee2 <ijlee2@users.noreply.github.com>
2024-08-15 12:54:20 +0200

    chore: Updated fixtures

---
+++ packages/blueprints-addon/tests/fixtures/run-generate-component/glimmer/output/packages/ui/button/src/components/my/example-1.ts
@@ -0,0 +1,15 @@
+import Component from '@glimmer/component';
+
+import styles from './example-1.css';
+
+interface MyExample1Signature {
+  Args: {};
+  Blocks: {
+    default: [];
+  };
+  Element: null;
+}
+
+export default class MyExample1Component extends Component<MyExample1Signature> {
+  styles = styles;
+}
