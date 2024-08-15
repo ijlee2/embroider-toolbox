5f09ff0926c35866d35870f0ce25323783d605f8
ijlee2 <ijlee2@users.noreply.github.com>
2024-08-15 16:17:29 +0200

    feature: Updated barrel file and template registry

--- packages/blueprints-addon/src/run-destroy.ts
+++ packages/blueprints-addon/src/run-destroy.ts
@@ -1,8 +1,15 @@
-import { createOptions, removeEntity } from './steps/run-destroy/index.js';
+import {
+  createOptions,
+  removeEntity,
+  updateBarrelFile,
+  updateTemplateRegistry,
+} from './steps/run-destroy/index.js';
 import type { CodemodOptions } from './types/run-destroy.js';
 
 export function runDestroy(codemodOptions: CodemodOptions): void {
   const options = createOptions(codemodOptions);
 
   removeEntity(options);
+  updateBarrelFile(options);
+  updateTemplateRegistry(options);
 }
--- packages/blueprints-addon/src/steps/run-destroy/index.ts
+++ packages/blueprints-addon/src/steps/run-destroy/index.ts
@@ -1,2 +1,4 @@
 export * from './create-options.js';
 export * from './remove-entity.js';
+export * from './update-barrel-file.js';
+export * from './update-template-registry.js';
