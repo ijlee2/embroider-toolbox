ed1c764aa25704e988cd9f4eec9fd5664486dd13
ijlee2 <ijlee2@users.noreply.github.com>
2024-08-16 13:36:02 +0200

    chore: Updated fixtures

---
+++ packages/blueprints-addon/tests/fixtures/run-new/input/docs-app/types/global.d.ts
@@ -0,0 +1,10 @@
+import '@glint/environment-ember-loose';
+import '@glint/environment-ember-template-imports';
+import 'ember-source/types';
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    // Add any registry entries from other addons here that your addon itself uses (in non-strict mode templates)
+    // See https://typed-ember.gitbook.io/glint/using-glint/ember/using-addons
+  }
+}
