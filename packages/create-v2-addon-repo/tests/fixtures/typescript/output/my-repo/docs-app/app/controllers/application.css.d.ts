9a9cc29b6567d4d71fc3d04e2731f50e06e6e0ff
ijlee2 <ijlee2@users.noreply.github.com>
2024-08-16 13:10:04 +0200

    chore: Updated fixtures

---
+++ packages/create-v2-addon-repo/tests/fixtures/typescript/output/my-repo/docs-app/app/controllers/application.css.d.ts
@@ -0,0 +1,9 @@
+declare const styles: {
+  readonly 'center': string;
+  readonly 'container': string;
+  readonly 'footer': string;
+  readonly 'header': string;
+  readonly 'main': string;
+};
+
+export default styles;
