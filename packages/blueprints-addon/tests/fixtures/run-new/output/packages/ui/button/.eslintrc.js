9fb677ebf997783c780f9b60d4d88a69627d9a02
ijlee2 <ijlee2@users.noreply.github.com>
2024-08-19 08:59:50 +0200

    chore: Updated fixtures (blueprints-addon)

--- packages/blueprints-addon/tests/fixtures/run-new/output/packages/ui/button/.eslintrc.js
+++ packages/blueprints-addon/tests/fixtures/run-new/output/packages/ui/button/.eslintrc.js
@@ -4,22 +4,4 @@ require('@shared-configs/eslint-config-ember/patch');
 
 module.exports = {
   extends: ['@shared-configs/eslint-config-ember/v2-addon'],
-  parserOptions: {
-    project: true,
-    tsconfigRootDir: __dirname,
-  },
-  overrides: [
-    // TypeScript files
-    {
-      files: ['**/*.{gts,ts}'],
-      rules: {
-        '@typescript-eslint/no-unnecessary-condition': 'error',
-      },
-    },
-    // JavaScript files
-    {
-      files: ['**/*.{cjs,js,gjs,mjs}'],
-      extends: ['plugin:@typescript-eslint/disable-type-checked'],
-    },
-  ],
 };
