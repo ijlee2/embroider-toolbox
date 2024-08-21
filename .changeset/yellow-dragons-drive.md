f4f79b962eba15ba5e700ce4ee1aa54447ea1bbc
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2024-08-21 14:15:11 +0200

    Introduced @embroider/broccoli-side-watch to test-app (#25)
    * bugfix: Introduced @embroider/broccoli-side-watch to test-app
    
    * chore: Updated fixtures
    
    * chore: Added changeset
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

---
+++ .changeset/yellow-dragons-drive.md
@@ -0,0 +1,5 @@
+---
+"create-v2-addon-repo": patch
+---
+
+Introduced @embroider/broccoli-side-watch to test-app
--- packages/create-v2-addon-repo/tests/fixtures/typescript/output/my-repo/test-app/ember-cli-build.js
+++ packages/create-v2-addon-repo/tests/fixtures/typescript/output/my-repo/test-app/ember-cli-build.js
@@ -1,15 +1,27 @@
 'use strict';
 
+const sideWatch = require('@embroider/broccoli-side-watch');
+const { maybeEmbroider } = require('@embroider/test-setup');
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
+    autoImport: {
+      watchDependencies: [],
+    },
+
     'ember-cli-babel': {
       enableTypeScriptTransform: true,
+    },
+
+    trees: {
+      app: sideWatch('app', {
+        watching: [],
+      }),
     },
 
     // Add options here
   });
 
-  return app.toTree();
+  return maybeEmbroider(app);
 };
