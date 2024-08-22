eae6550587e119551c34c0f5b1ea0e5673a17ff5
ijlee2 <ijlee2@users.noreply.github.com>
2024-08-22 12:52:58 +0200

    refactor: Combined steps to reduce runtime (in-class)

--- packages/analyze-ember-project-dependencies/src/utils/find-dependencies/in-class/index.ts
+++ packages/analyze-ember-project-dependencies/src/utils/find-dependencies/in-class/index.ts
@@ -1,4 +1,2 @@
-export * from './find-export-statements.js';
-export * from './find-import-statements.js';
-export * from './find-require-statements.js';
+export * from './find-modules.js';
 export * from './find-services.js';
