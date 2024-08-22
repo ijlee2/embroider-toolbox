0ff69f0b059be07ef120b38dd2d2e7a8af537c68
ijlee2 <ijlee2@users.noreply.github.com>
2024-08-22 10:54:02 +0200

    feature: Found entities that live outside the repo

--- packages/analyze-ember-project-dependencies/src/steps/find-entities.ts
+++ packages/analyze-ember-project-dependencies/src/steps/find-entities.ts
@@ -1,8 +1,34 @@
-import type { Entities, ProjectData } from '../types/index.js';
-import { findEntitiesFromRepo } from './find-entities/index.js';
+import type { Entities, EntityType, ProjectData } from '../types/index.js';
+import {
+  findEntitiesFromOutside,
+  findEntitiesFromRepo,
+} from './find-entities/index.js';
+
+function mergeEntities(
+  entitiesFromOutside: Entities,
+  entitiesFromRepo: Entities,
+): Entities {
+  function mergeByType(entityType: EntityType): Map<string, string> {
+    return new Map(
+      [
+        ...entitiesFromOutside[entityType],
+        ...entitiesFromRepo[entityType],
+      ].sort(),
+    );
+  }
+
+  return {
+    components: mergeByType('components'),
+    componentsDoubleColonized: mergeByType('componentsDoubleColonized'),
+    helpers: mergeByType('helpers'),
+    modifiers: mergeByType('modifiers'),
+    services: mergeByType('services'),
+  };
+}
 
 export function findEntities(projectData: ProjectData): Entities {
+  const entitiesFromOutside = findEntitiesFromOutside();
   const entitiesFromRepo = findEntitiesFromRepo(projectData);
 
-  return entitiesFromRepo;
+  return mergeEntities(entitiesFromOutside, entitiesFromRepo);
 }
