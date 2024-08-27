dce3086beee792d139c523db535db2d62813f173
ijlee2 <ijlee2@users.noreply.github.com>
2024-08-27 22:13:49 +0200

    feature: Analyzed .gjs and .gts files

--- packages/analyze-ember-project-dependencies/src/utils/find-dependencies/index.ts
+++ packages/analyze-ember-project-dependencies/src/utils/find-dependencies/index.ts
@@ -1,17 +1,22 @@
 import type { Entities, PackageAnalysis } from '../../types/index.js';
 import { analyzeClass } from './analyze-class.js';
 import { analyzeTemplate } from './analyze-template.js';
+import { analyzeTemplateTag } from './analyze-template-tag.js';
 
 export const patterns = {
-  app: ['app/**/*.{hbs,js,ts}', 'tests/**/*.{js,ts}', '*.{js,ts}'],
-  node: ['**/*.{js,ts}', '*.{js,ts}'],
-  'v1-addon': [
-    'addon/**/*.{hbs,js,ts}',
-    'addon-test-support/**/*.{js,ts}',
-    'tests/**/*.{js,ts}',
+  app: [
+    'app/**/*.{gjs,gts,hbs,js,ts}',
+    'tests/**/*.{gjs,gts,js,ts}',
     '*.{js,ts}',
   ],
-  'v2-addon': ['src/**/*.{hbs,js,ts}', '*.{cjs,js,mjs,ts}'],
+  node: ['**/*.{js,ts}', '*.{js,ts}'],
+  'v1-addon': [
+    'addon/**/*.{gjs,gts,hbs,js,ts}',
+    'addon-test-support/**/*.{js,ts}',
+    'tests/**/*.{gjs,gts,js,ts}',
+    '*.{js,ts}',
+  ],
+  'v2-addon': ['src/**/*.{gjs,gts,hbs,js,ts}', '*.{cjs,js,mjs,ts}'],
 };
 
 export type Data = {
@@ -22,6 +27,10 @@ export type Data = {
 
 export function analyzeFile(file: string, data: Data): PackageAnalysis {
   const { filePath } = data;
+
+  if (filePath.endsWith('.gjs') || filePath.endsWith('.gts')) {
+    return analyzeTemplateTag(file, data);
+  }
 
   if (filePath.endsWith('.hbs')) {
     return analyzeTemplate(file, data);
