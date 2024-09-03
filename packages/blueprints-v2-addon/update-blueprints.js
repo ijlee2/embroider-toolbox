e9211c5e27f8dd8e32b95ca2afd38c85453a9d89
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2024-09-03 10:49:16 +0200

    Update dependencies (#41)
    * chore: Updated development dependencies
    
    * chore: Added lockfile
    
    * chore: Updated dependencies in blueprints
    
    * chore: Updated CURRENT_VERSION
    
    * chore: Updated fixtures
    
    * chore: Added changeset
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- packages/blueprints-v2-addon/update-blueprints.js
+++ packages/blueprints-v2-addon/update-blueprints.js
@@ -5,7 +5,7 @@ import gitDiffApply from 'git-diff-apply';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
-const CURRENT_VERSION = '0.1.2';
+const CURRENT_VERSION = '0.1.3';
 
 async function updateBlueprints({ from, to }) {
   const startTag = from;
