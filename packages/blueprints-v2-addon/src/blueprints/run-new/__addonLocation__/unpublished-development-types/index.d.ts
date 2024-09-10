5436eaf88af9b72d75be3aa71677af4f130d72ce
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2024-09-10 06:18:54 +0200

    Set up @ember/test-helpers and qunit-dom (#49)
    * chore: Removed the directories field (unused)
    
    * feature: Set up @ember/test-helpers and qunit-dom
    
    * chore: Updated fixtures
    
    * chore: Updated CURRENT_VERSION
    
    * chore: Ran update-blueprints-v2-addon
    
    * chore: Added changeset
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- packages/blueprints-v2-addon/src/blueprints/run-new/__addonLocation__/unpublished-development-types/index.d.ts
+++ packages/blueprints-v2-addon/src/blueprints/run-new/__addonLocation__/unpublished-development-types/index.d.ts
@@ -4,6 +4,7 @@
 import '@glint/environment-ember-loose';
 import '@glint/environment-ember-template-imports';
 import 'ember-source/types';
+import 'qunit-dom';
 
 import type EmbroiderCssModulesRegistry from 'embroider-css-modules/template-registry';
 
