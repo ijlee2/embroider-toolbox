37382417fcc9e73ffd8adcfb481e56a7a2c8e94a
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-01-13 12:59:33 +0100

    Reverted #65 (#66)
    Revert "Updated dependencies in blueprints (#65)"
    
    This reverts commit c7439eb7b8a06700359c6f87862816c9a9d57c1a.
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- packages/create-v2-addon-repo/src/blueprints/test-app/tests/test-helper.ts
+++ packages/create-v2-addon-repo/src/blueprints/test-app/tests/test-helper.ts
@@ -1,6 +1,5 @@
 import { setApplication } from '@ember/test-helpers';
-import { setupEmberOnerrorValidation, start } from 'ember-qunit';
-import { loadTests } from 'ember-qunit/test-loader';
+import { start } from 'ember-qunit';
 import * as QUnit from 'qunit';
 import { setup } from 'qunit-dom';
 import Application from 'test-app/app';
@@ -9,6 +8,5 @@ import config from 'test-app/config/environment';
 setApplication(Application.create(config.APP));
 
 setup(QUnit.assert);
-setupEmberOnerrorValidation();
-loadTests();
+
 start();
--- packages/create-v2-addon-repo/tests/fixtures/typescript/output/my-repo/docs-app/tests/test-helper.ts
+++ packages/create-v2-addon-repo/tests/fixtures/typescript/output/my-repo/docs-app/tests/test-helper.ts
@@ -1,14 +1,12 @@
 import { setApplication } from '@ember/test-helpers';
 import Application from 'docs-app/app';
 import config from 'docs-app/config/environment';
-import { setupEmberOnerrorValidation, start } from 'ember-qunit';
-import { loadTests } from 'ember-qunit/test-loader';
+import { start } from 'ember-qunit';
 import * as QUnit from 'qunit';
 import { setup } from 'qunit-dom';
 
 setApplication(Application.create(config.APP));
 
 setup(QUnit.assert);
-setupEmberOnerrorValidation();
-loadTests();
+
 start();
