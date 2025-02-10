46c41ba67adb73e324e4089a35d9b5bffa6b66a4
ijlee2 <ijlee2@users.noreply.github.com>
2025-02-10 14:42:33 +0100

    chore: Ignored type errors

--- packages/analyze-ember-project-dependencies/eslint.config.mjs
+++ packages/analyze-ember-project-dependencies/eslint.config.mjs
@@ -13,4 +13,11 @@ export default [
     ],
   },
   ...baseConfiguration,
+  {
+    files: ['**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+    },
+  },
 ];
