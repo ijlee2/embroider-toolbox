761644e4c9d476b44d53e41b46984fb7aea85b8b
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-02-10 14:45:37 +0100

    Simplified lint configurations (#71)
    * chore: Consumed @ijlee2-frontend-configs
    
    * chore: Updated project configurations
    
    * chore: Updated local configs
    
    * chore: Added lockfile
    
    * chore: Ignored type errors
    
    * chore: Added changeset
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- packages/analyze-ember-project-dependencies/eslint.config.mjs
+++ packages/analyze-ember-project-dependencies/eslint.config.mjs
@@ -16,9 +16,7 @@ export default [
   {
     files: ['**/*.ts'],
     rules: {
-      '@typescript-eslint/no-unsafe-argument': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
-      '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
     },
   },
