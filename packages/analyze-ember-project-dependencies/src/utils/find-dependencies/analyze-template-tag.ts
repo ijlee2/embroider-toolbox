7524e596c66027fa20e3144f836c868edf7151e6
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-09-08 16:17:27 +0200

    refactor: Replaced content-tag with @codemod-utils/ast-template-tag

--- packages/analyze-ember-project-dependencies/src/utils/find-dependencies/analyze-template-tag.ts
+++ packages/analyze-ember-project-dependencies/src/utils/find-dependencies/analyze-template-tag.ts
@@ -1,15 +1,18 @@
+import { preprocess } from '@codemod-utils/ast-template-tag';
+
 import type { PackageAnalysis } from '../../types/index.js';
-import { extractClass } from '../ast/template-tag.js';
 import { analyzeClass } from './analyze-class.js';
 import type { Data } from './index.js';
 
 export function analyzeTemplateTag(file: string, data: Data): PackageAnalysis {
-  const newFile = extractClass(file);
+  const { javascript } = preprocess(file);
 
-  const newData = {
+  const filePath = data.filePath
+    .replace(/\.gjs$/, '.js')
+    .replace(/\.gts$/, '.ts');
+
+  return analyzeClass(javascript, {
     ...data,
-    filePath: data.filePath.replace(/\.gjs$/, '.js').replace(/\.gts$/, '.ts'),
-  };
-
-  return analyzeClass(newFile, newData);
+    filePath,
+  });
 }
