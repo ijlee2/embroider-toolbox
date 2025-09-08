8d38beef8e772b03ccceb7abe9f200f032a4de3a
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-09-08 16:19:22 +0200

    Installed @codemod-utils/ast-template-tag  (#97)
    * refactor: Replaced content-tag with @codemod-utils/ast-template-tag
    
    * chore: Added lockfile
    
    * chore: Added changeset

--- packages/analyze-ember-project-dependencies/src/utils/ast/template-tag.ts
+++
@@ -1,7 +0,0 @@
-import { Preprocessor } from 'content-tag';
-
-export function extractClass(file: string): string {
-  const preprocessor = new Preprocessor();
-
-  return preprocessor.process(file).code;
-}
